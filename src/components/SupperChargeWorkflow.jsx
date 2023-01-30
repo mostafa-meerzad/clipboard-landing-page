d2b853221f87a0370f478c04a99a1808d5bcf897
mostafa <mostafameerzad@gmail.com>
2023-01-30 13:10:08 +0430

    Correct the order of elements and (IBM) brand name

--- src/components/SupperChargeWorkflow.jsx
+++ src/components/SupperChargeWorkflow.jsx
@@ -8,10 +8,11 @@ import vector from "../assets/logo-vector-graphics.png";
 import { Text, BlackList, Preview } from "../icons";
 const brands = [
   { img: google, name: "Google" },
+  { img: ibm, name: "IBM" },
   { img: ms, name: "Microsoft" },
   { img: hp, name: "HP" },
-  { img: vector, name: "Vector Graphics" },
-  { img: ibm, name: "IMB" }
+  { img: vector, name: "Vector-Graphics" }
+
 ];
 const options = [
   {
@@ -33,9 +34,9 @@ const options = [
 const SupperChargeWorkflow = () => {
   return (
     <section className="supper-charge-workflow">
-      <h3 className="supper-charge-workflow__title">
+      <h2 className="supper-charge-workflow__title">
         Supercharge your workflow
-      </h3>
+      </h2>
       <p className="supper-charge-workflow__desc">
         We’ve got the tools to boost your productivity.
       </p>
@@ -44,9 +45,10 @@ const SupperChargeWorkflow = () => {
         {options.map(({ title, desc, img }, index) => {
           return (
             <li key={index} className="supper-charge-workflow__option">
+               {img}
               <h3 className="supper-charge-workflow__option-title">{title}</h3>
               <p className="supper-charge-workflow__option-desc">{desc}</p>
-              {img}
+             
             </li>
           );
         })}
@@ -56,7 +58,7 @@ const SupperChargeWorkflow = () => {
         {brands.map(({ img, name }, index) => {
           return (
             <li key={index} className="supper-charge-workflow__brand">
-              <img src={img} alt={name} />
+              <img src={img} alt={name} className={`supper-charge-workflow__brand-${name}`} />
             </li>
           );
         })}
